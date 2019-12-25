625a202e8a8567a27d614c1f885f07129c16a44b
g4rb4g3 <g4rb4g3@gmail.com>
2019-12-25 21:14:55 +0100

    completed release chooser and downloader

--- app/src/main/java/g4rb4g3/at/abrptransmittercompanion/MainActivity.java
+++ app/src/main/java/g4rb4g3/at/abrptransmittercompanion/MainActivity.java
@@ -8,15 +8,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -25,18 +31,23 @@ import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.Socket;
 import java.net.URL;
+import java.util.LinkedHashMap;
 
 public class MainActivity extends AppCompatActivity {
 
   public static final String TAG = "ABRPTransmitter";
   public static final String ABETTERROUTEPLANNER_URL_TOKEN = "token";
   public static final String ABRPTRANSMITTER_RELEASE_URL = "https://api.github.com/repos/g4rb4g3/abrptransmitter/releases";
+  public static final String APK_NAME = "app-debug.apk";
 
   private static final int EXCHANGE_PORT = 6942;
 
-  AbrpTransmitterServer mAbrpTransmitterServer = new AbrpTransmitterServer(8080);
+  private AbrpTransmitterServer mAbrpTransmitterServer = new AbrpTransmitterServer(8080);
 
-  ProgressDialog mProgressDialog;
+  private ProgressDialog mProgressDialog;
+  private Spinner mSpReleases;
+  private LinkedHashMap<String, String> mReleases;
+  private String mApkPath = null;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +58,22 @@ public class MainActivity extends AppCompatActivity {
     final Context context = getApplicationContext();
     final EditText edCompanionIp = findViewById(R.id.ed_companion_ip);
     final EditText edToken = findViewById(R.id.ed_abrp_token);
+    mSpReleases = findViewById(R.id.sp_releases);
+    mSpReleases.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+      @Override
+      public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String url = mReleases.get(mSpReleases.getSelectedItem().toString());
+        if("".equals(url)) {
+          return;
+        }
+        new AbrpTransmitterReleaseLoader().execute(url);
+      }
+
+      @Override
+      public void onNothingSelected(AdapterView<?> parent) {
+
+      }
+    });
     Button btSend = findViewById(R.id.btn_send);
     btSend.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -119,17 +146,39 @@ public class MainActivity extends AppCompatActivity {
       try {
         URL url = new URL(params[0]);
         connection = (HttpURLConnection) url.openConnection();
-        connection.connect();
-        InputStream stream = connection.getInputStream();
-        reader = new BufferedReader(new InputStreamReader(stream));
-        StringBuffer buffer = new StringBuffer();
-        String line;
+        if (params[0].equals(ABRPTRANSMITTER_RELEASE_URL)) {
+          connection.connect();
+          InputStream stream = connection.getInputStream();
+          reader = new BufferedReader(new InputStreamReader(stream));
+          StringBuffer buffer = new StringBuffer();
+          String line;
 
-        while ((line = reader.readLine()) != null) {
-          buffer.append(line);
+          while ((line = reader.readLine()) != null) {
+            buffer.append(line);
 
+          }
+          return buffer.toString();
+        } else {
+          connection.connect();
+
+          if(connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+
+            InputStream stream = connection.getInputStream();
+
+            File outDir = getExternalFilesDir(null);
+            File outFile = new File(outDir, APK_NAME);
+            FileOutputStream fileOutputStream = new FileOutputStream(outFile);
+            byte[] buffer = new byte[1024];
+            int bufferLength;
+
+            while ((bufferLength = stream.read(buffer)) > 0) {
+              fileOutputStream.write(buffer, 0, bufferLength);
+            }
+            fileOutputStream.close();
+
+            return outFile.getAbsolutePath();
+          }
         }
-        return buffer.toString();
       } catch (IOException e) {
         Log.e(TAG, e.getMessage(), e);
       } finally {
@@ -150,6 +199,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPostExecute(String result) {
       super.onPostExecute(result);
+      if(result.startsWith("[") && result.endsWith("]")) {
+        try {
+          JSONArray jsonArray = new JSONArray(result);
+          mReleases = new LinkedHashMap<>(jsonArray.length()+1, 1.0f);
+          mReleases.put(getString(R.string.choose_release), "");
+          String[] spinnerContent = new String[jsonArray.length()];
+          for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String name = jsonObject.getString("name");
+            String url = jsonObject.getJSONArray("assets").getJSONObject(0).getString("browser_download_url");
+            mReleases.put(name, url);
+            spinnerContent[i] = name;
+          }
+          ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, mReleases.keySet().toArray(new String[mReleases.keySet().size()]));
+          mSpReleases.setAdapter(adapter);
+        } catch (JSONException e) {
+          Log.e(TAG, "error getting github releases", e);
+        }
+      } else {
+        mApkPath = result;
+      }
       if (mProgressDialog.isShowing()) {
         mProgressDialog.dismiss();
       }
